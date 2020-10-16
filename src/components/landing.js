2c0a75b572061dd493e0a7fa3de042f0f40e774c
grace000 <tiffani.grace.elmore@gmail.com>
2020-10-16 02:36:36 -0500

    remove landing image

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,36 +4,13 @@ import { useStaticQuery, graphql } from "gatsby"
 import LandingData from "../data/landing.json"
 
 const Landing = () => {
-  const { title, subtitle, image } = LandingData
-
-  const data = useStaticQuery(graphql`
-    query {
-      allFile {
-        nodes {
-          childImageSharp {
-            fluid(maxWidth: 700, maxHeight: 500) {
-              originalName
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const images = data.allFile.nodes
-  const landingImage = images.find(
-    i => i.childImageSharp.fluid.originalName === image
-  )
+  const { title, subtitle } = LandingData
 
   return (
     <div className="landing-wrapper">
       <div className="landing-text">
         <h1 className="landing-title">{title}</h1>
         <p className="landing-subtitle">{subtitle}</p>
-      </div>
-      <div className="landing-image">
-        <Img fluid={landingImage.childImageSharp.fluid} />
       </div>
     </div>
   )
