8e14d8aebdf0ffd1051469a031ee73ef9ee45006
grace000 <tiffani.grace.elmore@gmail.com>
2020-10-19 21:46:18 -0500

    remove image

--- src/components/engage.js
+++ src/components/engage.js
@@ -5,35 +5,12 @@ import EngageData from "../data/engage.json"
 import CardWithTextLink from "./cards/card-with-text-link"
 
 const Engage = () => {
-  const { title, tiles, image } = EngageData
-
-  const data = useStaticQuery(graphql`
-    query {
-      allFile {
-        nodes {
-          childImageSharp {
-            fluid(maxWidth: 500, maxHeight: 320) {
-              originalName
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const images = data.allFile.nodes
-  const engageImage = images.find(
-    i => i.childImageSharp.fluid.originalName === image
-  )
+  const { title, tiles } = EngageData
 
   return (
     <div id="Engage" className="engage-wrapper">
       <h2 className="engage-title">{title}</h2>
       <div className="engage-cards-wrapper">
-        <div className="engage-image">
-          <Img fluid={engageImage.childImageSharp.fluid} />
-        </div>
         {/* <div className="engage-cards"> */}
         {tiles.map((tile, index) => (
           <CardWithTextLink key={index} content={tile} />
