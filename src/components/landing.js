d2da35c3c81c193d2fc301d397edc791b052ccfd
grace000 <tiffani.grace.elmore@gmail.com>
2020-10-22 04:56:25 -0500

    add animated css to landing

--- src/components/landing.js
+++ src/components/landing.js
@@ -2,12 +2,14 @@ import React from "react"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 import LandingData from "../data/landing.json"
+import Floaters from "./floaters"
 
 const Landing = () => {
   const { title, subtitle } = LandingData
 
   return (
     <div className="landing-wrapper">
+      <Floaters />
       <div className="landing-text">
         <h1 className="landing-title">{title}</h1>
         <p className="landing-subtitle">{subtitle}</p>
