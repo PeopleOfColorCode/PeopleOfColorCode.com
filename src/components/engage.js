f462a720627bb3de9ccb21d46847cb24ea0a7f8e
grace000 <tiffani.grace.elmore@gmail.com>
2020-10-23 02:56:21 -0500

    engage cards padding responsive

--- src/components/engage.js
+++ src/components/engage.js
@@ -1,19 +1,19 @@
 import React from "react"
 import EngageData from "../data/engage.json"
 import ContentCardCubed from "./cards/content-card-cubed"
+import Floaters from "./floaters"
 
 const Engage = () => {
   const { title, tiles } = EngageData
 
   return (
     <div id="Engage" className="engage-wrapper">
+      <Floaters />
       <h2 className="engage-title">{title}</h2>
       <div className="engage-cards-wrapper">
-        {/* <div className="engage-cards"> */}
         {tiles.map((tile, index) => (
           <ContentCardCubed key={index} content={tile} border />
         ))}
-        {/* </div> */}
       </div>
     </div>
   )
