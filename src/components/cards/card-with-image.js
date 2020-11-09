11780e7d450da2592a4abc221b26f21f6fbe4dfd
grace000 <tiffani.grace.elmore@gmail.com>
2020-11-09 02:17:26 -0600

    add aria label to external link anchor

--- src/components/cards/card-with-image.js
+++ src/components/cards/card-with-image.js
@@ -29,7 +29,13 @@ const CardWithImage = ({ content: { image, name, url } }) => {
       <Img fluid={cardImage.childImageSharp.fluid} />
       <div className="card-with-image-content">
         <h3 className="card-with-image-header">{name}</h3>
-        <a className="link-logo" href={url} target="_blank" />
+        <a
+          className="link-logo"
+          aria-label="External Link"
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+        />
       </div>
     </div>
   )
